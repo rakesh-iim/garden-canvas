d7032ee9f83f7377d99e01f2b98cee294eeb0c61
rakesh-iim <rakesh-iim@users.noreply.github.com>
2026-05-08 12:14:24 +0530

    chore: remove vercel config and update server.ts for latest tanstack api

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
-import { getRouterManifest } from "@tanstack/react-start/router-manifest";
 import {
   createStartHandler,
   defaultStreamHandler,
 } from "@tanstack/react-start/server";
-import { getRouter } from "./router";
 
-export default createStartHandler({
-  createRouter: getRouter,
-  getRouterManifest,
-})(defaultStreamHandler);
+const handler = createStartHandler(defaultStreamHandler);
+
+export default {
+  fetch: handler
+};
